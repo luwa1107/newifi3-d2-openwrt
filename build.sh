6c69184337222adb2fc6f6a232c796e198758a85
luwa <52826837+luwa1107@users.noreply.github.com>
2024-01-17 15:33:30 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,6 +13,9 @@ export OP_BUILD_PATH=$PWD
 git clone https://github.com/coolsnowwolf/lede.git
 
 cd "${OP_BUILD_PATH}"/lede || exit
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
 ./scripts/feeds update -a && ./scripts/feeds install -a
 rm -rf ./tmp && rm -rf .config
 mv "${OP_BUILD_PATH}"/.config "${OP_BUILD_PATH}"/lede/.config
